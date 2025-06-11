82dff95bf9e53860704eeb1bc2e9a0917ad52e1b
Vydeuh <samuel.f.vidal@gmail.com>
2025-06-11 12:13:07 +0200

    Ajout des tests sur l'horaire + quelques modifications de code + modification des tests suite au passage de JUnit4 à JUnit5 et Hamcrest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,21 @@
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
     public static void main(String[] args) {
         String maPhrase = "salut";
-        String heure = obtenirHeure();
-        System.out.println(bonjour());
         System.out.println(palindrome(maPhrase));
-        System.out.println(auRevoir());
-        System.out.println(heure);
     }
+
+    static LocalTime FIN_DE_NUIT = LocalTime.of(6, 0, 0);
+    static LocalTime FIN_DU_JOUR = LocalTime.of(18, 0, 0);
+    static LocalTime INSTANT = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
 
     public static String palindrome(String maPhrase) {
         StringBuilder inverse = new StringBuilder(maPhrase).reverse();
         String reponse = inverse.toString();
-        if(maPhrase.equals("")) {
+        if(maPhrase.isEmpty()) {
             return bonjour() + auRevoir();
         }
         else if (maPhrase.equals(reponse)) {
@@ -25,14 +25,11 @@ public class Main {
         }
     }
 
-    public static String obtenirHeure() {
-        LocalTime heure = LocalTime.now();
-        DateTimeFormatter heureFormatee = DateTimeFormatter.ofPattern("HH:mm:ss");
-        return heure.format(heureFormatee);
-    }
-
     private static String bonjour() {
-        return "Bonjour\n";
+        if(INSTANT.isAfter(FIN_DE_NUIT) && INSTANT.isBefore(FIN_DU_JOUR.plusSeconds(1))) {
+            return "Bonjour\n";
+        }
+        return "Bonsoir\n";
     }
 
     private static String auRevoir() {
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,42 +1,36 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.time.LocalTime;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestMain {
 
+    // Tests sur les chaînes de caractères
     @Test
-    public void testPalindromeRenvoitMotALEnvers() {
-        String unePhrase = "bouh";
-        String resultat = Main.palindrome(unePhrase);
-        assertEquals("Bonjour\nhuob\nAu revoir", resultat);
+    public void testPalindromeRenvoieMotALEnvers() {
+        String resultat = Main.palindrome("bouh");
+        assertThat(resultat, containsString("huob"));
     }
 
     @Test
-    public void testPalindromeRenvoitPalindrome() {
-        String unePhrase = "anna";
-        String resultat = Main.palindrome(unePhrase);
-        assertEquals("Bonjour\nbien dit !\nAu revoir", resultat);
+    public void testPalindromeRenvoiePalindrome() {
+        String resultat = Main.palindrome("anna");
+        assertThat(resultat, containsString("bien dit !"));
     }
 
     @Test
     public void testPlusieursMotsSontBienRenvoyesALEnvers() {
-        String unePhrase = "Bibi la Bibiste";
-        String resultat = Main.palindrome(unePhrase);
-        assertEquals("Bonjour\netsibiB al ibiB\nAu revoir", resultat);
+        String resultat = Main.palindrome("Bibi la Bibiste");
+        assertThat(resultat, containsString("etsibiB al ibiB"));
     }
 
     @Test
     public void testBienDitALEnversDoitBienRenvoyerBienDit() {
-        String unePhrase = "! tid neib";
-        String resultat = Main.palindrome(unePhrase);
-        assertEquals("Bonjour\nbien dit !\nAu revoir", resultat);
-    }
-
-    @Test
-    public void testUnNonPalindromeNeRenvoitPasBienDit() {
-        String unePhrase = "Salut";
-        String resultat = Main.palindrome(unePhrase);
-        assertNotEquals("Bonjour\nbien dit !\nAu revoir", resultat);
+        String resultat = Main.palindrome("! tid neib");
+        assertThat(resultat, containsString("bien dit !"));
     }
 
     @Test
@@ -44,6 +38,47 @@ public class TestMain {
         String unePhrase = "";
         String resultat = Main.palindrome(unePhrase);
         assertEquals("Bonjour\n\nAu revoir", resultat);
+    }
+
+    @Test
+    public void testPalindromeCommenceParBonjour() {
+        String resultat = Main.palindrome("Salut");
+        assertThat(resultat, startsWith("Bonjour\n"));
+    }
+
+    @Test
+    public void testPalindromeTermineParAuRevoir() {
+        String resultat = Main.palindrome("Salut");
+        assertThat(resultat, endsWith("\nAu revoir"));
+    }
+
+    // Tests sur l'heure
+    @Test
+    public void testPalindromeRenvoieBonjourDe6h00m01sA18h00m00s() {
+        Main.INSTANT = Main.FIN_DE_NUIT.plusSeconds(1);
+        String resultat = Main.palindrome("En journée");
+        assertThat(resultat, containsString("Bonjour\n"));
+    }
+
+    @Test
+    public void testPalindromeRenvoieEncoreBonjourA18h00Pile() {
+        Main.INSTANT = Main.FIN_DU_JOUR;
+        String resultat = Main.palindrome("En journée");
+        assertThat(resultat, containsString("Bonjour\n"));
+    }
+
+    @Test
+    public void testPalindromeRenvoieBonsoirDe18h00m01sA6h00m00s() {
+        Main.INSTANT = Main.FIN_DU_JOUR.plusSeconds(1);
+        String resultat = Main.palindrome("En soirée");
+        assertThat(resultat, containsString("Bonsoir\n"));
+    }
+
+    @Test
+    public void testPalindromeRenvoieEncoreBonsoirA6h00Pile() {
+        Main.INSTANT = Main.FIN_DE_NUIT;
+        String resultat = Main.palindrome("En journée");
+        assertThat(resultat, containsString("Bonsoir\n"));
     }
 }
 
